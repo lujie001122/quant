1ae2214cf2408b8ec7db2359670039f1b3da3764
LuJie <lujie001122@qq.com>
2024-12-22 20:43:51 +0800

    8
    8

---
+++ CommInfoFractional.py
@@ -0,0 +1,5 @@
+import backtrader as bt
+class CommInfoFractional(bt.CommissionInfo):
+    def getsize(self, price, cash):
+        '''Returns fractional size for cash operation @price'''
+        return self.p.leverage * (cash / price)
--- study/Lesson2.py
+++ study/Lesson2.py
@@ -6,6 +6,9 @@ import numpy as np
 import pandas as pd
 import talib
 
+from CommInfoFractional import CommInfoFractional
+
+
 class SignalData(bt.feeds.PandasData):
     lines = ('signal','rsi_value')
     params = (
@@ -14,8 +17,8 @@ class SignalData(bt.feeds.PandasData):
               )
 class SimpleCrossStrategy(bt.Strategy):
     params = (
-        ('sma_fast_period', 5),  # 缩短快速移动平均线周期
-        ('sma_slow_period', 10),  # 缩短慢速移动平均线周期
+        ('sma_fast_period', 7),  # 缩短快速移动平均线周期
+        ('sma_slow_period', 13),  # 缩短慢速移动平均线周期
         ('boll_period', 20),
         ('boll_std', 2),
         ('trade_size', 20000)
@@ -28,11 +31,18 @@ class SimpleCrossStrategy(bt.Strategy):
         self.rsi_value = self.datas[0].rsi_value
 
     def next(self):
-        if not self.position and self.sma5[0] > self.sma10[0] and self.sma5[-1] <= self.sma10[-1] and (self.rsi_value[0] > 30 and self.rsi_value[0] < 70 ):#(self.rsi_value[0] > 30 and self.rsi_value[0] < 70 ):
-            self.order = self.buy(size=self.params.trade_size)
-        if (self.position and self.sma5[0] < self.sma10[0] and self.sma5[-1] >= self.sma10[-1]) or self.rsi_value[0] > 70:
-            # 卖出当前所有持仓
-            self.order = self.sell(size=self.position.size)
+        # 获取当前数据的收盘价
+        close_price = self.data.close[0]
+        if not self.position and self.sma5[0] > self.sma10[0] and self.sma5[-1] <= self.sma10[-1] and (self.rsi_value[0] > 20 and self.rsi_value[0] < 90 ):#(self.rsi_value[0] > 30 and self.rsi_value[0] < 70 ):
+            # 获取当前可用现金
+            cash = self.broker.getcash()*0.8
+            # 计算可购买的比特币数量，考虑到可能有小数精度问题，使用 round 函数
+            size = round(cash / close_price, 8)
+            self.order = self.buy(size=size)
+        if (self.position and self.sma5[0] < self.sma10[0] and self.sma5[-1] >= self.sma10[-1]) or self.rsi_value[0] > 90:
+            # 获取当前持仓量
+            size = self.getposition(self.data).size
+            self.order = self.sell(size=size)
 
     def log(self, txt, dt=None):
         # 记录策略的执行日志
@@ -64,6 +74,7 @@ class SimpleCrossStrategy(bt.Strategy):
             self.bar_executed = len(self)
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('订单取消/保证金不足/拒绝')
+            print(order)
 
             # 其他状态记录为：无挂起订单
         self.order = None
@@ -110,7 +121,7 @@ def main():
     timeframe = '15m'
     df = fetch_ohlcv(exchange, symbol, timeframe)
 
-    start_cash = 1500000.0
+    start_cash = 10000.0
     cerebro = bt.Cerebro()
     data = SignalData(dataname=df)
     cerebro.adddata(data)
@@ -119,6 +130,9 @@ def main():
     cerebro.addstrategy(SimpleCrossStrategy)
     cerebro.broker.setcash(start_cash)
     cerebro.broker.setcommission(commission=0.0001)
+    # comminfo = CommInfoFractional(commission=0.0001)
+    # cerebro.broker.addcommissioninfo(comminfo)
+    # cerebro.broker.set_checksubmit(False)  # 避免下单时检查保证金，仅用于回测
     # 运行回测
     cerebro.run()
     # 输出回测结果
@@ -126,7 +140,7 @@ def main():
     print('回测结束后的总资金: %.2f' % cerebro.broker.getvalue())
     print('净收益: %.2f' % (cerebro.broker.getvalue() - start_cash))
     # 画图
-    # cerebro.plot()
+    cerebro.plot()
 
 if __name__ == "__main__":
     main()
